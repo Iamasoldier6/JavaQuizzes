fccd7a5d5dd47694b770de9936b7d31c13c5b754
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-08-28 11:24:30 +0800

    类似击沉战舰的游戏

---
+++ DotComGame/src/DotComGame.java
@@ -0,0 +1,23 @@
+public class DotComGame {
+	public static void main(String[] args) {
+		int numOfGuesses = 0;
+		GameHelper helper = new GameHelper();
+
+		DotCom dotCom = new DotCom();
+		int randomNum = (int) (Math.random() * 5);
+
+		int[] locations = { randomNum, randomNum + 1, randomNum + 2 };
+		dotCom.setLocationCells(locations);
+		boolean isAlive = true;
+
+		while (isAlive == true) {
+			String guess = helper.getUserInput("enter a number");
+			String result = dotCom.checkYourself(guess);
+			numOfGuesses++;
+			if (result.equals("kill")) {
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " guesses");
+			}
+		}
+	}
+}
